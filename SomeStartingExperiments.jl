022842417979a8bc107d55a58c4271a9fc0ccd76
Francis Smart <francis.smart@gmail.com>
2019-09-25 17:19:00 -0400

    Update SomeStartingExperiments.jl

--- SomeStartingExperiments.jl
+++ SomeStartingExperiments.jl
@@ -40,22 +40,61 @@ k2p = plot(x=k2, Geom.histogram)
 
 cor(ordinalrank(k1), ordinalrank(w))
 
-function f(x; skewtarget = .5, kurttarget = 1)
-    z = (w.*(1 - sum(x)) + w1*x[1] + w2*x[2] + k1*x[3] + k2*x[4])
+bounded(x) = min(1, max(0, x))
+
+standardize!(x::Array) = x[:] = (x .- mean(x))./var(x)^.5
+
+# standardize!(w)
+# standardize!(w1)
+# standardize!(w2)
+# standardize!(k1)
+# standardize!(k2)
+
+function f(x; skewtarget = .35, kurttarget = .4)
+    χ = x .|> bounded |> x -> [bounded(1 - sum(x)), x...]
+    z = w.*χ[1] + w1.*χ[2] + w2.*χ[3] + k1.*χ[4] + k2.*χ[5]
+    (skewtarget - skewness(z))^2 + (kurttarget - kurtosis(z))^2
+end
+
+
+skewness.([w1, w2, k1, k2])
+kurtosis.([w1, w2, k1,k2])
+
+x0 = [0.0, 0.0, 0.0, 0.0]
+b = optimize(f, x0)
+Optim.minimizer(b) .|> bounded
+
+w3 = Optim.minimizer(b) .|> bounded |> x -> (w.*(1 - sum(x)) + w1*x[1] + w2*x[2] + k1*x[3] + k2*x[4])
+
+skewness(w3), kurtosis(w3)
+
+plot(x=w3, Geom.histogram(bincount=100))
+
+cor(ordinalrank(w),ordinalrank(w3))
+
+################### polyscaler
+
+polyscaler(x, scale) = sign.(x) .* (abs.(x .+ sign.(x))).^scale .- sign.(x) 
+
+cor(polyscaler(w, .03) |> ordinalrank, w |> ordinalrank)
+
+x -> polyscaler(x,.0001) |> x -> (skewness(x), kurtosis(x))
+
+plot(x -> polyscaler(x,.0001), -1, 1)
+
+function F(x; skewtarget = 1.5, kurttarget = -1)
+    χ = [1 - sum(x), x...]
+    z = w.*χ[1] + w1.*χ[2] + w2.*χ[3] + k1.*χ[4] + k2.*χ[5]
     (skewtarget - skewness(z))^2 + (kurttarget - kurtosis(z))^2
 end
 
 x0 = [0.0, 0.0, 0.0, 0.0]
-b = optimize(f, x0)
+b = optimize(F, x0)
 Optim.minimizer(b)
 
 w3 = Optim.minimizer(b) |> x -> (w.*(1 - sum(x)) + w1*x[1] + w2*x[2] + k1*x[3] + k2*x[4])
 
 skewness(w3), kurtosis(w3)
+plot(x=w3, Geom.histogram(bincount=100))
 
 cor(ordinalrank(w),ordinalrank(w3))
-
-# lower = [0, 0, 0, 0]
-# upper = [1, 1, 1, 1]
-# b2 = optimize(f, lower, upper, x0)
-#  optimize(f, lower, upper, Brent())
